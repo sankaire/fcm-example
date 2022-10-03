9fb0226e9b279170c57a9c63d4f00bd8a7f6fcb7
sankaire <kerenketepela@gmail.com>
2022-10-03 17:49:46 +0300

    chore:👀just an example of where to call your push notification fn

---
+++ src/controllers/pussExample.controller.ts
@@ -0,0 +1,15 @@
+import {sendPushNotification, sendTransactionPushNotification} from "../utils/services/pushNotification";
+import {Request,Response} from "express";
+
+export const topicPushExample = async (req:Request, res:Response) =>{
+    //do something
+    //
+    // sendPushNotification(params)
+    return
+}
+export const tokenPushExample = async (req:Request, res:Response) =>{
+    //do something
+    //
+    // sendTransactionPushNotification(params)
+    return
+}
